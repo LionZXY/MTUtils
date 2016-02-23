09897e530b8a441e8bc94a0e6818efc7be440b43
LionZXY <nikita@kulikof.ru>
2016-02-23 19:25:01 +0300

    1.4.1

--- src/main/java/ru/lionzxy/damagetweaker/NBTMT.java
+++ src/main/java/ru/lionzxy/damagetweaker/NBTMT.java
@@ -1,12 +1,17 @@
 package ru.lionzxy.damagetweaker;
 
 import minetweaker.api.data.*;
+import minetweaker.api.formatting.IFormattedText;
+import minetweaker.api.item.IItemStack;
+import minetweaker.api.tooltip.IngredientTooltips;
+import minetweaker.mc1710.item.MCItemStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * ru.lionzxy.damagetweaker
@@ -15,6 +20,91 @@ import java.util.List;
  */
 @ZenClass("mods.MTUtilsNBT")
 public class NBTMT {
+
+    @ZenMethod
+    public static void addMultilineShiftTooltip(IItemStack stack, IFormattedText strs, String s) {
+        for (String str : splitString(MTHandlers.getStringFromFormattedText(strs), s))
+            IngredientTooltips.addShiftTooltip(stack, MTHandlers.getIFormatedTextFromString(str));
+    }
+
+    @ZenMethod
+    public static void addMultilineTooltip(IItemStack stack, IFormattedText strs, String s) {
+        for (String str : splitString(MTHandlers.getStringFromFormattedText(strs), s))
+            IngredientTooltips.addTooltip(stack, MTHandlers.getIFormatedTextFromString(str));
+    }
+
+    @ZenMethod
+    public static String[] splitString(String original, char split) {
+        return spilitToByte(original, (byte) split);
+    }
+
+    @ZenMethod
+    public static String[] splitString(String original, String split) {
+        return original.split(split);
+    }
+
+    @ZenMethod
+    public static Map readMapFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asMap();
+    }
+
+    @ZenMethod
+    public static short readShortFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asShort();
+    }
+
+    @ZenMethod
+    public static String readStringFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asString();
+    }
+
+    @ZenMethod
+    public static int readIntFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asInt();
+    }
+
+
+    @ZenMethod
+    public static int[] readIntArrayFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asIntArray();
+    }
+
+
+    @ZenMethod
+    public static List readListFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asList();
+    }
+
+
+    @ZenMethod
+    public static long readLongFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asLong();
+    }
+
+    @ZenMethod
+    public static byte readByteFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asByte();
+    }
+
+    @ZenMethod
+    public static byte[] readByteArrayFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asByteArray();
+    }
+
+    @ZenMethod
+    public static double readDoubleFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asDouble();
+    }
+
+    @ZenMethod
+    public static float readFloatFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asFloat();
+    }
+
+    @ZenMethod
+    public static boolean readBoolFromNBT(IData nbt, String path) {
+        return getUnReadDataFromString(nbt, path).asBool();
+    }
 
     public static IData generateChangeData(IData d) {
         if (d instanceof DataMap) {
@@ -160,6 +250,17 @@ public class NBTMT {
             merge(datas[i], datas[datas.length - 1]);
         }
         return datas[datas.length - 1];
+    }
+
+    @ZenMethod
+    public static IData getUnReadDataFromString(IData nbt, String path) {
+        IData toExit = nbt;
+        String[] pathToPoint = spilitToByte(path, (byte) '.');
+        for (int i = 0; i < pathToPoint.length; i++) {
+            toExit = toExit.asMap().get(pathToPoint[i]);
+        }
+
+        return toExit;
     }
 
     static boolean merge(IData d, IData d2) {
